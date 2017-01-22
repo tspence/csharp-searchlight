8332e12d81a8fc710efbaaf1b09ffa08f9ca88a7
ted-spence <ted@spence.net>
2017-01-21 18:54:34 -0800

    Work towards establishing project as NetStandard1.6

--- src/Searchlight/Properties/AssemblyInfo.cs
+++ src/Searchlight/Properties/AssemblyInfo.cs
@@ -2,32 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Searchlight")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Searchlight")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cb9aca0e-046b-4508-8674-e4eb8fe72bd5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("beafc1bf-9bee-4806-8ca0-f0c7cf74db28")]
---
+++ tests/Searchlight.Tests/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Xunit;
+
+namespace Searchlight.Tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        //[Fact]
+        public void Test1()
+        {
+            //Assert.True(new Searchlight.Class1().IsTrue());
+        }
+    }
+}
