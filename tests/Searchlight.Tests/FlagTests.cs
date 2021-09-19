021f26fdbb6c5aca59d50c8d249961cebcbb1aa0
Ted Spence <ted@spence.net>
2021-09-19 11:17:45 -0700

    Added support for flags using the "Include" parameter (#45)
    * Added support for flags using the "Include" parameter
    
    Relocated attributes to their own folder
    Removed the unused "Preview" system
    Added a new error for "IncludeNotFound" so it can be different from FieldNotFound
    Eliminated the entire "MatchesName" system which executed lots of unnecessary code for each alias, in favor of an aliasing system with predictable performance using a string dictionary
    Added optional "HasFlag" choice you can use to detect whether a flag was turned on using an "$include" parameter
    
    * Updated nuspec files
    
    Added some descriptions of the change

---
+++ tests/Searchlight.Tests/FlagTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searchlight.Tests
+{
+    [TestClass]
+    public class FlagTests
+    {
+        [SearchlightModel]
+        [SearchlightFlag(Name = "TestFlag", Aliases = new string[] { "AliasOne", "AliasTwo" })]
+        public class FlagTestClass
+        {
+        }
+
+        [TestMethod]
+        public void TestFlagEnable()
+        {
+            var engine = new SearchlightEngine()
+               .AddClass(typeof(FlagTestClass));
+
+            // Test whether the flag can be found by its name
+            var syntax = engine.Parse(new FetchRequest()
+            {
+                include = "TestFlag",
+                table = "FlagTestClass"
+            });
+            Assert.AreEqual(1, syntax.Flags.Count);
+            Assert.AreEqual("TestFlag", syntax.Flags[0].Name);
+            Assert.AreEqual(true, syntax.HasFlag("TestFlag"));
+
+            // Test whether the flag can be found by its alias
+            syntax = engine.Parse(new FetchRequest()
+            {
+                include = "AliasOne",
+                table = "FlagTestClass"
+            });
+            Assert.AreEqual(1, syntax.Flags.Count);
+            Assert.AreEqual("TestFlag", syntax.Flags[0].Name);
+            Assert.AreEqual(true, syntax.HasFlag("TestFlag"));
+            syntax = engine.Parse(new FetchRequest()
+            {
+                include = "AliasTwo",
+                table = "FlagTestClass"
+            });
+            Assert.AreEqual(1, syntax.Flags.Count);
+            Assert.AreEqual("TestFlag", syntax.Flags[0].Name);
+            Assert.AreEqual(true, syntax.HasFlag("TestFlag"));
+
+            // Test whether the flag is false by default
+            syntax = engine.Parse(new FetchRequest()
+            {
+                table = "FlagTestClass"
+            });
+            Assert.AreEqual(0, syntax.Flags.Count);
+            Assert.AreEqual(false, syntax.HasFlag("TestFlag"));
+
+            // Test whether an unknown flag throws the correct error
+            var ex = Assert.ThrowsException<IncludeNotFound>(() =>
+            {
+                engine.Parse(new FetchRequest()
+                {
+                    table = "FlagTestClass",
+                    include = "TestFlag, AnUnknownAlias, AnotherUnknownAliasButThisOneDoesntGetTestedBecauseTheFirstOneFails",
+                });
+            });
+            Assert.AreEqual("AnUnknownAlias", ex.IncludeName);
+            Assert.AreEqual("TestFlag, AnUnknownAlias, AnotherUnknownAliasButThisOneDoesntGetTestedBecauseTheFirstOneFails", ex.OriginalInclude);
+            Assert.IsTrue(ex.KnownIncludes.Contains("TestFlag"));
+        }
+    }
+}
