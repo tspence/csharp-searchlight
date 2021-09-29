b937cc09810e91b5f9d886cc2f9aeddce61df249
CTylerD <ctdennis@protonmail.com>
2021-09-29 15:03:08 -0700

    Updated Searchlight Engine Tests

--- tests/Searchlight.Tests/EngineTests.cs
+++ tests/Searchlight.Tests/EngineTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Searchlight.Exceptions;
 
 namespace Searchlight.Tests
 {
@@ -23,6 +24,35 @@ namespace Searchlight.Tests
             Assert.IsNotNull(engine.FindTable("TestAlias2"));
             Assert.IsNotNull(engine.FindTable("TestTableAliases"));
             Assert.AreEqual(engine.FindTable("TestTableAliases"), engine.FindTable("TestAlias1"));
+        }
+        
+        [TestMethod]
+        public void Test_MaxPSInitialized()
+        {
+            var engine = new SearchlightEngine();
+            Assert.IsTrue(SearchlightEngine.MaximumPageSize == 1000);
+        }
+
+        private FetchRequest mockFetchRequest = new FetchRequest
+        {
+            table = "tableau",
+            include = "include me",
+            filter = "",
+        };
+
+        [TestMethod]
+        public void Test_SettingRequestPageSizeWhenNotSpecified()
+        {
+            var engine = new SearchlightEngine();
+            engine.Parse(mockFetchRequest);
+            Assert.IsTrue(mockFetchRequest.pageSize == SearchlightEngine.MaximumPageSize); }
+
+        [TestMethod]
+        public void Test_ParsingRequestWithInvalidPageSize()
+        {
+            mockFetchRequest.pageSize = 2000; // default MaxPS is 1000
+            var engine = new SearchlightEngine();
+            Assert.ThrowsException<InvalidPageSize>(() => engine.Parse(mockFetchRequest));
         }
     }
 }
