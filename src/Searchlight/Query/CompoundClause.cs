f8be93129e9a7a604669a3f733e4f84424043388
Ted Spence <ted@spence.net>
2023-08-19 20:10:05 -0700

    Add some more tests
    We can also simplify the ToString method for compound clause

--- src/Searchlight/Query/CompoundClause.cs
+++ src/Searchlight/Query/CompoundClause.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Searchlight.Query
@@ -18,25 +20,10 @@ namespace Searchlight.Query
         /// </summary>
         public override string ToString()
         {
-            // TODO: This piece of code raises a worry.  What happens if you mix AND and OR within a compound
-            // clause?  e.g. (A and B or C) - what order will they be parsed in?  A and (b or c)? (a and b) or c?
-            // We should make this deterministic and require that all clauses within a parenthesis have the same
-            // conjunction.
-            var sb = new StringBuilder();
-            sb.Append("(");
-            var numChildren = Children?.Count ?? 0;
-            for (var i = 0; i < numChildren; i++)
-            {
-                sb.Append(Children[i]);
-                if (i < numChildren)
-                {
-                    sb.Append(" ");
-                    sb.Append(Children[i].Conjunction);
-                    sb.Append(" ");
-                }
-            }
-            sb.Append(")");
-            return sb.ToString();
+            // After release 1.0.0 we can assert that all conjunctions within a compound clause are identical
+            var conjunction = $" {Children[0].Conjunction} ";
+            var clauses = Children.Select(c => c.ToString()).ToArray();
+            return $"({string.Join(conjunction, clauses)})";
         }
     }
 }
